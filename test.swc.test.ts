4fb6be8ca16565b5b6b032ec5b6e4ce8270c40a5
chris <chris@bumblehead.com>
2024-07-13 01:11:11 -0700

    add additional test importing main-ts.ts

--- test.swc.test.ts
+++ test.swc.test.ts
@@ -1,8 +1,20 @@
 import assert from 'node:assert'
 import test from 'node:test'
+import esmock from 'esmock'
 
 test('should import babelGeneratedDoubleDefault, cjs deep', async () => {
   const esmmain1 = await import ('./src/esmmain1.js')
 
   assert.ok('babelGeneratedDoubleDefaultResult' in esmmain1.default)
+})
+
+test('should import ts when using tsx', async () => {
+  // const main = await import ('./src/main-ts.ts')
+  const main = await esmock('./src/main-ts.ts', {
+    path: {
+      basename: () => 'hellow'
+    }
+  })
+
+  assert.strictEqual(main.pathbasenamewrap(), 'hellow')  
 })
--- test.tsx.test.ts
+++ test.tsx.test.ts
@@ -1,8 +1,19 @@
 import assert from 'node:assert'
 import test from 'node:test'
+import esmock from 'esmock'
 
 test('should import babelGeneratedDoubleDefault, cjs deep', async () => {
   const esmmain1 = await import ('./src/esmmain1.js')
 
   assert.ok('babelGeneratedDoubleDefaultResult' in esmmain1.default)
+})
+
+test('should import ts when using tsx', async () => {
+  const main = await esmock('./src/main-ts.ts', {
+    path: {
+      basename: () => 'hellow'
+    }
+  })
+
+  assert.strictEqual(main.pathbasenamewrap(), 'hellow')  
 })
